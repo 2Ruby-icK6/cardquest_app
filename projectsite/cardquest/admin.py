7a23a3b986580cd49e54c8809f73ad558dfea26a
2Ruby-icK6 <202180232@psu.palawan.edu.ph>
2023-12-15 08:05:34 +0800

    fix __str__

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -3,19 +3,19 @@ from .models import PokemonCard, Trainer, Collection
 
 # Display all fields from models including created_at and updated_at
 class BaseModelAdmin(admin.ModelAdmin):
-    list_display = ('created_at', 'update_at')
+    list_display = ('created_at', 'updated_at')
 
 class TrainerAdmin(BaseModelAdmin):
-    list_display = ('name', 'birthdate', 'location', 'email', 'created_at', 'update_at')
-
+    list_display = ('name', 'birthdate', 'location', 'email', 'created_at', 'updated_at')
+    
 class PokemonCardAdmin(BaseModelAdmin):
     list_display = (
         'name', 'rarity', 'hp', 'card_type', 'attack', 'description', 'weakness',
-        'card_number', 'release_date', 'evolution_stage', 'abilities', 'created_at', 'update_at'
+        'card_number', 'release_date', 'evolution_stage', 'abilities', 'created_at', 'updated_at'
     )
 
 class CollectionAdmin(BaseModelAdmin):
-    list_display = ('card', 'Trainer', 'collection_date', 'created_at', 'update_at')
+    list_display = ('card', 'trainer', 'collection_date', 'created_at', 'updated_at')
 
 # Register your models here.
 admin.site.register(PokemonCard, PokemonCardAdmin)
--- projectsite/cardquest/models.py
+++ projectsite/cardquest/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class BaseModel (models.Model) :
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
-    update_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
@@ -55,8 +55,14 @@ class PokemonCard (BaseModel) :
     release_date = models.DateField(null=True, blank=True)
     evolution_stage = models.CharField(max_length=250, null=True, blank=True)
     abilities = models.CharField(max_length=250, null=True, blank=True)
+
+    def __str__(self) :
+        return self.name
         
 class Collection (BaseModel) :
     card = models.ForeignKey(PokemonCard, blank=True, null=True, on_delete=models.CASCADE)
-    Trainer = models.ForeignKey(Trainer, blank=True, on_delete=models.CASCADE)
+    trainer = models.ForeignKey(Trainer, blank=True, on_delete=models.CASCADE)
     collection_date = models.DateField(null=True, blank=True)
+
+    def __str__(self) :
+        return self.name
